0dc9f5b08c1c937cdb2ec5082e42271893ce9340
3107Alok <3107aloksingh@gmail.com>
2026-02-09 14:39:02 +0530

    Add files via upload

---
+++ Alok_wt/file1.js
@@ -0,0 +1,5 @@
+let user1="Admin";
+function showUser(){
+    console.log("User is:",user1);
+}
+showUser();
---
+++ Alok_wt/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+   // let grtr=(a,b,c)=>{
+    //     if((a>b && a>c)? console.log(a," is greater"):((b>c && b>a)? console.log(b," is greater"):console.log(c," is greater")));}
+    // grtr(2,4,6);
