daee9e337887fb569ec2b5269a3d040d817715d5
3107Alok <3107aloksingh@gmail.com>
2026-02-16 14:50:21 +0530

    Add files via upload

---
+++ Alok_wt/alok.dart
@@ -0,0 +1,66 @@
+/* void main() {
+  print('Hello, World!');
+}    */
+
+
+
+//Calculator 
+
+/* import 'dart:io';
+
+double add(double a, double b) {
+  return a + b;
+}
+double sub(double a, double b) {
+  return a - b;
+}
+double prod(double a, double b) {
+  return a * b;
+}
+double div(double a, double b) {
+  if (b == 0) {
+    print("Divide by 0");
+    return 0;
+  }
+  return a / b;
+}
+
+void main() {
+
+  stdout.write("Input a: ");
+  double a = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Input b: ");
+  double b = double.parse(stdin.readLineSync()!);
+
+  print("Your Choice 1. Add, 2. Subtract, 3. Multiply, Divide");
+  stdout.write("Enter 1-4 ");
+  int choice = int.parse(stdin.readLineSync()!);
+
+  double result;
+  switch (choice) {
+    case 1:
+      result = add(a, b);
+      print("Result = $result");
+      break;
+    case 2:
+      result = sub(a, b);
+      print("Result = $result");
+      break;
+    case 3:
+      result = prod(a, b);
+      print("Result = $result");
+      break;
+    case 4:
+      result = div(a, b);
+      print("Result = $result");
+      break;
+    default:
+      print("Invalid choice");
+  }
+} 
+ */
+
+
+
+
